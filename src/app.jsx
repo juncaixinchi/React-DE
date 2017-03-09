e215fcbae17f79e7bc1f11fc36d42e9c597028f2
lixinwei <xinwei.li@winsuntech.cn>
2017-03-09 09:02:17 +0800

    Modify the congfig
    modified:   .babelrc
    modified:   .eslintrc

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { CatSilhouette, BallOfYarn, Account, ReportProblem, HDDIcon, RAIDIcon, UpIcon, DownIcon
+} from './svg'
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello React aaa!</h1>
-    </div>
-  );
-}
+const App = () => (
+  <div className="container">
+    <h1>Hello React !</h1>
+    <MuiThemeProvider>
+      <Account />
+    </MuiThemeProvider>
+  </div>
+)
 
-const app = document.createElement('div');
-document.body.appendChild(app);
-ReactDOM.render(<App />, app);
+const app = document.createElement('div')
+document.body.appendChild(app)
+ReactDOM.render(<App />, app)
