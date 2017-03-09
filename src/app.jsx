6d4bc6f4829d3db29ee4dfd75b8d06a3c95080c3
lixinwei <xinwei.li@winsuntech.cn>
2017-03-09 13:15:31 +0800

    Add npm-shrinkwrap.json
    modified:   .eslintrc
    new file:   npm-shrinkwrap.json
    deleted:    src/Maintenance.jsx
    modified:   src/app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Toggle } from 'material-ui'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import { CatSilhouette, BallOfYarn, Account, ReportProblem, HDDIcon, RAIDIcon, UpIcon, DownIcon
 } from './svg'
 
 const App = () => (
   <div className="container">
-    <h1>Hello React !</h1>
     <MuiThemeProvider>
-      <Account />
+      <div>
+        <h1>Hello React !</h1>
+        <Toggle />
+        <CatSilhouette />
+      </div>
     </MuiThemeProvider>
   </div>
 )
